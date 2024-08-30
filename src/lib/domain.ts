07a7a822a16669c693927b9305504e2a4bcb4b84
jarrodschilling <jarrodschilling@gmail.com>
2024-08-30 08:45:34 -0600

    domain swap

--- src/lib/domain.ts
+++ src/lib/domain.ts
@@ -1,5 +1,5 @@
 // Make sure to switch for deployment
 
-export const app_domain = "http://localhost:3000"
+// export const app_domain = "http://localhost:3000"
 
-// export const app_domain = process.env.SITE_URL || ''
+export const app_domain = process.env.SITE_URL || ''
