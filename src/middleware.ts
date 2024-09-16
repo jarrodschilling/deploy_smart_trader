333d30354d597a026e28d7e24bf68802ca814193
jarrodschilling <jarrodschilling@gmail.com>
2024-09-16 08:47:30 -0600

    middleware fixed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,35 @@
-export { default } from "next-auth/middleware"
+import { getToken } from 'next-auth/jwt';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-export const config = { matcher: [
-    "/dashboard",
-    "/current-portfolio",
-    "/transactions",
-    "/equity-curve",
-    "/trade-sheet",
-    "/trade-statistics",
-    "/users",
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  const { pathname } = req.nextUrl;
+  const publicPaths = ['/', '/login', '/register'];
 
-]}
+  if (publicPaths.includes(pathname) || token) {
+    return NextResponse.next();
+  }
+  return NextResponse.redirect(new URL('/login', req.url));
+}
 
-// export const config = { matcher: ["/((?!register|api/auth|login|$).*)"]}
+export const config = {
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+};
 
-// export const config = { matcher: ['/((?!login|register|api/auth|$).*)']}
 
 
+// export { default } from "next-auth/middleware"
 
+// export const config = { matcher: [
+//     "/dashboard",
+//     "/current-portfolio",
+//     "/transactions",
+//     "/equity-curve",
+//     "/trade-sheet",
+//     "/trade-statistics",
+//     "/users",
 
-
-// export default function middleware() {
-
-// }
+// ]}
